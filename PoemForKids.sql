935e98cd93ea99fba2ca2605519b591650964fb0
anna-moore <ammoore434@gmail.com>
2021-03-15 10:17:34 -0500

    complete

--- PoemForKids.sql
+++ PoemForKids.sql
@@ -85,7 +85,6 @@ SELECT count(pe.Id) [total sadness poem]
 JOIN Emotion e ON e.id = pe.PoemId
 WHERE e.Name = 'sadness';
 
-
 --17 How many poems are not associated with any emotion?
 SELECT count(p.id) 'emotionless poem'
 	FROM Poem p
@@ -93,10 +92,30 @@ LEFT JOIN PoemEmotion pe on p.id = pe.PoemId
 WHERE pe.PoemId IS NULL;
 
 --18 Which emotion is associated with the least number of poems?
-
+select top 1 count(pe.emotionId) 'emotion count', e.Name 'emotion cited the least'
+from PoemEmotion pe
+join Emotion e on pe.EmotionId = e.id
+group by pe.EmotionId, e.Name
+order by 'emotion count';
 
 --19 Which grade has the largest number of poems with an emotion of joy?
-
+select top 1 count(pe.id) 'happy poems', g.Name
+from PoemEmotion pe
+join Emotion e on pe.EmotionId = e.id
+join Poem p on pe.PoemId = p.id
+join Author a on a.id = p.AuthorId
+join Grade g on g.id = a.GradeId
+where e.Name = 'joy'
+group by g.Name
+order by 'happy poems' desc;
 
 --20 Which gender has the least number of poems with an emotion of fear?
-
+select count(pe.id) 'fearful poems', g.Name
+from PoemEmotion pe
+join Emotion e on pe.EmotionId = e.id
+join Poem p on pe.PoemId = p.id
+join Author a on a.id = p.AuthorId
+join Gender g on g.id = a.GenderId
+where e.Name = 'fear'
+group by g.Name
+order by 'fearful poems';
